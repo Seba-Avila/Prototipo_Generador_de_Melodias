706b76b1cb2472a7d68a4f3d0b3b3a7f7a8a430e
Seba-Avila <142743699+Seba-Avila@users.noreply.github.com>
2023-10-04 18:11:52 -0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
+#include "arduinoFFT.h"
 #include "BluetoothSerial.h"
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
+arduinoFFT FFT;
+
+const uint16_t samples = 512; //This value MUST ALWAYS be a power of 2
+const double samplingFrequency = 5000; //Hz, must be less than 10000 due to ADC
+
+unsigned int sampling_period_us;
+unsigned long microseconds;
+
+double vReal[samples];
+double vImag[samples];
 
 char buffer[5]; //declaro como vector porque la funcion readBytesUntil() solo toma de argumento un vector de tipo char o byte
 byte cache;     //declaro como byte la variable donde guardar los datos devueltos por la funcion readBytesUntil(), que solamente corresponde con la cantidad de bytes leidos
@@ -17,206 +28,56 @@ void Detector(); //funcion correspondiente a la pantalla del Detector de Notas. 
 
 //Declaraciones Accion
 
-byte muestras[200] PROGMEM={127,
-130,
-134,
-138,
+byte muestras[50] PROGMEM={127,
 142,
-146,
-150,
-154,
 158,
-162,
-166,
-170,
 173,
-177,
-181,
-184,
 188,
-191,
-195,
-198,
 201,
-204,
-207,
-210,
 213,
-216,
-219,
-222,
 224,
-227,
-229,
-232,
 234,
-236,
-238,
-240,
 241,
-243,
-245,
-246,
 247,
-248,
-250,
-250,
 251,
-252,
-252,
 253,
 253,
-253,
-254,
-253,
-253,
-253,
-252,
-252,
 251,
-250,
-250,
-248,
 247,
-246,
-245,
-243,
 241,
-240,
-238,
-236,
 234,
-232,
-229,
-227,
 224,
-222,
-219,
-216,
 213,
-210,
-207,
-204,
 201,
-198,
-195,
-191,
 188,
-184,
-181,
-177,
 173,
-170,
-166,
-162,
 158,
-154,
-150,
-146,
 142,
-138,
-134,
-130,
-126,
-123,
-119,
-115,
+127,
 111,
-107,
-103,
-99,
 95,
-91,
-87,
-83,
 80,
-76,
-72,
-69,
 65,
-62,
-58,
-55,
 52,
-49,
-46,
-43,
 40,
-37,
-34,
-31,
 29,
-26,
-24,
-21,
 19,
-17,
-15,
-13,
 12,
-10,
-8,
-7,
 6,
-5,
-3,
-3,
 2,
-1,
-1,
 0,
 0,
-0,
-0,
-0,
-0,
-0,
-1,
-1,
 2,
-3,
-3,
-5,
 6,
-7,
-8,
-10,
 12,
-13,
-15,
-17,
 19,
-21,
-24,
-26,
 29,
-31,
-34,
-37,
 40,
-43,
-46,
-49,
 52,
-55,
-58,
-62,
 65,
-69,
-72,
-76,
 80,
-83,
-87,
-91,
 95,
-99,
-103,
-107,
-111,
-115,
-119,
-123}; 
+111}; 
 
 /*Las siguientes son las funciones donde se ejecutara cada PWM en base a las muestras antes definidas*/
 void SenoidalC();
@@ -232,7 +93,7 @@ void SenoidalA();
 void SenoidalBb();
 void SenoidalB();
 
-void Microfono(); //funcion que ejecuta la accion prototipo del Detector de notas.
+void Microfono(); //funcion que ejecuta la accion del Detector de notas.
 
 void setup() {
   Serial.begin(115200);
@@ -241,15 +102,10 @@ void setup() {
 
   /*Más aclaraciones en archivo README*/
     
-  ledcSetup(0, 200000, 8); //Aqui configuro la frecuencia y resolucion del canal PWM que voy a utilizar
-  ledcAttachPin(19 , 0);   //Aqui asigno los canales configurados a un pin de la placa
-
-  /*Aqui configuro y asigno a pin el canal de la PWM para el detector de nota. Posee una resolucion de 8
-  bits a diferencia de la resolucion de 12 del ADC ya que permite que la diferencia de bits entre el umbral y los
-  picos captados sea mas notoria en la salida al LED debido a la mayor distancia en voltaje entre muestra y muestra 
-  para una menor resolucion*/
-  ledcSetup(1, 200000, 8);  
-  ledcAttachPin(15, 1);
+  ledcSetup(0, 10000, 8); //Aqui configuro la frecuencia y resolucion del canal PWM que voy a utilizar
+  ledcAttachPin(32 , 0);   //Aqui asigno el canal configurados a un pin de la placa
+  
+  ledcWrite(0, 255);
 
 
   Serial.println("\n\nInicio");  
@@ -369,155 +225,165 @@ void Detector(){
 void SenoidalC(){
     Serial.println("Senoidal C");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(144.51);
+            delayMicroseconds(599.06);
         }
     }
-    ledcWrite(0, 0);                    //al romperse el bucle, la salida se pone a 0.
+    ledcWrite(0, 255);                    //al romperse el bucle, la salida se pone a 1 (estado inactivo para el optoacoplador).
 }
 
 void SenoidalDb(){
   Serial.println("Senoidal c");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(135.85);
+            delayMicroseconds(564.43);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalD(){
   Serial.println("Senoidal D");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(128.13);
+            delayMicroseconds(533.54);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalEb(){
   Serial.println("Senoidal d");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(121.2);
+            delayMicroseconds(505.82);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalE(){
   Serial.println("Senoidal E");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(114.95);
+            delayMicroseconds(480.8);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalF(){
   Serial.println("Senoidal F");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(106.63);
+            delayMicroseconds(447.54);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalGb(){
   Serial.println("Senoidal f");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(101.69);
+            delayMicroseconds(427.78);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalG(){
   Serial.println("Senoidal G");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(95.04);
+            delayMicroseconds(401.16);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalAb(){
   Serial.println("Senoidal g");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(89.15);
+            delayMicroseconds(377.61);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalA(){
   Serial.println("Senoidal A");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(83.9);
+            delayMicroseconds(356.64);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalBb(){
   Serial.println("Senoidal a");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(79.2);
+            delayMicroseconds(337.83);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 void SenoidalB(){
   Serial.println("Senoidal B");
     while(!SerialBT.available()){
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 50; j++) {
             ledcWrite(0, muestras[j]);
-            delayMicroseconds(73.64);
+            delayMicroseconds(315.58);
         }
     }
-    ledcWrite(0, 0);
+    ledcWrite(0, 255);
 }
 
 
 
 
 void Microfono(){
-    int Res;                                      //declaro una variable en la que guardar la diferencia entre umbral y picos captados
-    Serial.println("Entre al bucle mic");
+    sampling_period_us = round(1000000*(1.0/samplingFrequency));
+
 
     while(buffer[0] != 'P' && buffer[0] != 'V'){  //mientras los datos recibidos no correspondan al boton de Parada o Volver
         while(!SerialBT.available()){             //ejecuto la siguiente acción mientras no se reciban datos
-            Res = 2000 - analogRead(36);          //habiendo configurado y medido el umbral, establecemos una diferencia entre ese valor y el valor leído
-            if(abs(Res)>150){                     //como el valor del umbral no es completamente estable, suelen haber variaciones minimas, pero si la diferencia supera los 150 bits
-                ledcWrite(1, abs(Res));           //enciendo el LED con un ciclo de trabajo correspondiente a la diferencia. El valor esta tomado como un absoluto porque los picos aparecen con valores tanto mayores como menores al umbral
-                delay(1);
-            }else{
-                ledcWrite(1, 0);                  //si esa diferencia no se cumple, apago la salida.
-            }              
-            Serial.println(analogRead(36));
-            delay(1);
+            microseconds = micros();
+            for(int i=0; i<samples; i++)
+            {
+                vReal[i] = analogRead(36);
+                vImag[i] = 0;
+                while(micros() - microseconds < sampling_period_us){}
+                microseconds += sampling_period_us;
+            }
+            FFT = arduinoFFT(vReal, vImag, samples, samplingFrequency);
+            FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
+            FFT.Compute(FFT_FORWARD); /* Compute FFT */
+            FFT.ComplexToMagnitude(); /* Compute magnitudes */
+            double x = FFT.MajorPeak();
+            Serial.println(x, 6);
+            int n = (int) x;
+            Serial.println(n);
+            SerialBT.print(n);
+
+            
+            delay(1000); 
         }
         cache=SerialBT.readBytesUntil('\n', buffer, 3); //leo el dato que rompió el bucle. Si es V, romperá tambien el bucle principal de Detector. Si es P, romperá solo este bucle y volverá a esperar que se presione boton de Comienzo
     }
-    ledcWrite(1, 0);                                    //apago la salida
 }
